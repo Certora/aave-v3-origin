ecc0cd649b0388cbb4a1184cb7b4f694cadc0c83
nisnislevi <nissan@certora.com>
2024-12-12 14:54:16 +0200

    remove solvency files, and prepare for PR

--- certora/experimental/specs/unresolved.spec
+++
@@ -1,4 +0,0 @@
-// summaries for unresolved calls
-methods {
-    // xxx may need a dispatcher list for flashLoan where PTA-for-linking failed 
-}
--- certora/solvency/harnesses/Aave/DummyContract.sol
+++
@@ -1,8 +0,0 @@
-
-contract DummyContract {
-    function havoc_all_dummy() external {
-      //        havoc_all_dummy_internal();
-    }
-
-    //function havoc_all_dummy_internal() internal {}
-}
